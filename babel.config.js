be2ccba2c304348db7c0d73a7f5d9c9bc349fb92
hiroakiyasa <hiroakiyasa@yahoo.co.jp>
2025-09-14 17:14:49 +0900

    fix(ios): use reanimated babel plugin to prevent release white screen

--- babel.config.js
+++ babel.config.js
@@ -31,8 +31,8 @@ module.exports = function(api) {
           verbose: false,
         },
       ],
-      // Worklets plugin must be listed last (replaces reanimated plugin)
-      'react-native-worklets/plugin',
+      // Reanimated plugin must be listed last
+      'react-native-reanimated/plugin',
     ],
   };
 };
